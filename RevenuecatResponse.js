8cc1d1119d7809d11e74a592c01b0f57352a0c6b
David Wang <131627176+miosx@users.noreply.github.com>
2023-12-06 15:43:34 +0800

    Update RevenuecatResponse.js

--- RevenuecatResponse.js
+++ RevenuecatResponse.js
@@ -13,7 +13,7 @@ let subscriber = () => {
   	body = body.replace('"entitlements":{},','"entitlements":{"premium":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"mf_20_lifetime2","purchase_date":"2023-01-07T09:26:18Z"}},');
   	body = body.replace('"non_subscriptions":{},','"non_subscriptions":{"mf_20_lifetime2":[{"is_sandbox":false,"original_purchase_date":"2021-09-01T12:00:00Z","purchase_date":"2021-09-01T12:00:00Z","store":"app_store"}]},');
   	$done({body});
-  } else if (userAgent.startsWith("Fileball")) {
+  } else if (userAgent.startsWith("Fileball") || userAgent.startsWith("Filebox")) {
   	body = body.replace('"entitlements":{},','"entitlements":{"filebox_pro":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"filebox_pro","purchase_date":"2023-01-07T09:26:18Z"}},');
   	body = body.replace('"non_subscriptions":{},','"non_subscriptions":{"filebox_pro":[{"is_sandbox":false,"original_purchase_date":"2021-09-01T12:00:00Z","purchase_date":"2021-09-01T12:00:00Z","store":"app_store"}]},');
   	$done({body});
