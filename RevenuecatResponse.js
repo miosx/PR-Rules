65ec82e8a03685b6025997aa5b66b3ef4c481762
David Wang <131627176+miosx@users.noreply.github.com>
2023-12-07 08:35:33 +0800

    Update RevenuecatResponse.js

--- RevenuecatResponse.js
+++ RevenuecatResponse.js
@@ -7,12 +7,13 @@ let subscriber = () => {
   	return;
   }
   let headers = $request.headers;
-  let userAgent = headers['user-agent'] || headers['User-Agent'];
+  var userAgent = headers['user-agent'] || headers['User-Agent'];
+  userAgent = userAgent.toLowerCase();
 //   console.log(userAgent);
   if (userAgent.startsWith("mizframa")) {
   	body = body.replace('"entitlements":{},','"entitlements":{"premium":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"mf_20_lifetime2","purchase_date":"2023-01-07T09:26:18Z"}},');
   	body = body.replace('"non_subscriptions":{},','"non_subscriptions":{"mf_20_lifetime2":[{"is_sandbox":false,"original_purchase_date":"2021-09-01T12:00:00Z","purchase_date":"2021-09-01T12:00:00Z","store":"app_store"}]},');
-  } else if (userAgent.startsWith("Fileball") || userAgent.startsWith("Filebox")) {
+  } else if (userAgent.startsWith("fileball") || userAgent.startsWith("filebox")) {
   	body = body.replace('"entitlements":{},','"entitlements":{"filebox_pro":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"filebox_pro","purchase_date":"2023-01-07T09:26:18Z"}},');
   	body = body.replace('"non_subscriptions":{},','"non_subscriptions":{"filebox_pro":[{"is_sandbox":false,"original_purchase_date":"2021-09-01T12:00:00Z","purchase_date":"2021-09-01T12:00:00Z","store":"app_store"}]},');
   } else if (userAgent.startsWith("aptv")) {
