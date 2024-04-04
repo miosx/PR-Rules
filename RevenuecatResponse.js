9c82a07884ecc9a34bcde0e1d34824707dd90d08
David Wang <131627176+miosx@users.noreply.github.com>
2024-04-04 13:04:37 +0800

    Update RevenuecatResponse.js

--- RevenuecatResponse.js
+++ RevenuecatResponse.js
@@ -19,6 +19,9 @@ let subscriber = () => {
   } else if (userAgent.startsWith("aptv")) {
   	body = body.replace('"entitlements":{},','"entitlements":{"pro":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"com.kimen.aptvpro.lifetime","purchase_date":"2023-01-07T09:26:18Z"}},');
   	body = body.replace('"non_subscriptions":{},','"non_subscriptions":{"com.kimen.aptvpro.lifetime":[{"is_sandbox":false,"original_purchase_date":"2021-09-01T12:00:00Z","purchase_date":"2021-09-01T12:00:00Z","store":"app_store"}]},');
+  } else if (userAgent.startsWith("shellboxkit")) {
+  	body = body.replace('"entitlements":{},','"entitlements":{"premium":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"ShellBoxKit.Lifetime","purchase_date":"2023-01-07T09:26:18Z"}},');
+  	body = body.replace('"non_subscriptions":{},','"non_subscriptions":{"ShellBoxKit.Lifetime":[{"is_sandbox":false,"original_purchase_date":"2021-09-01T12:00:00Z","purchase_date":"2021-09-01T12:00:00Z","store":"app_store"}]},');
   }
   $done({body});
 };
