6a38e7746bafe435d3177c3310441629ead03e5e
David Wang <131627176+miosx@users.noreply.github.com>
2024-07-12 13:04:38 +0800

    Update RevenuecatResponse.js

--- RevenuecatResponse.js
+++ RevenuecatResponse.js
@@ -33,7 +33,7 @@ let subscriber = () => {
   	body = body.replace('"non_subscriptions":{},','"non_subscriptions":{"com.tk.client.lifetime":[{"is_sandbox":false,"original_purchase_date":"2021-09-01T12:00:00Z","purchase_date":"2021-09-01T12:00:00Z","store":"app_store"}]},');
   } else if (userAgent.startsWith("picseedclient")) {
   	body = body.replace('"entitlements":{},','"entitlements":{"Pro":{"expires_date":"2033-05-18T03:33:19Z","grace_period_expires_date":null,"product_identifier":"com.picseed.sub.pro.annual","purchase_date":"2023-01-07T09:26:18Z"}},');
-  	body = body.replace('"subscriptions":{},','"subscriptions":{"com.picseed.sub.pro.annual":[{"auto_resume_date":null,"billing_issues_detected_at":null,"expires_date":"2033-05-18T03:33:19Z","grace_period_expires_date":null,"is_sandbox":false,"original_purchase_date":"2023-01-07T09:26:21Z","ownership_type":"PURCHASED","period_type":"normal","purchase_date":"2023-01-07T09:26:18Z","store":"app_store","store_transaction_id":"190001505827904"}]},');
+  	body = body.replace('"subscriptions":{}','"subscriptions":{"com.picseed.sub.pro.annual":[{"auto_resume_date":null,"billing_issues_detected_at":null,"expires_date":"2033-05-18T03:33:19Z","grace_period_expires_date":null,"is_sandbox":false,"original_purchase_date":"2023-01-07T09:26:21Z","ownership_type":"PURCHASED","period_type":"normal","purchase_date":"2023-01-07T09:26:18Z","store":"app_store","store_transaction_id":"190001505827904"}]}');
   }
   $done({body});
 };
